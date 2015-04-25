faf5bb4b7894a5da9c4ac87dbf92f93e4e9b9043
raspibox <raffael.eiler@de.bosch.com>
2015-04-25 17:08:40 +0200

    Changes in get_KWL.py

--- heizung/get_KWL.py
+++ heizung/get_KWL.py
@@ -9,25 +9,36 @@ import RPi.GPIO as GPIO
 import time
 import sys
 
+debug = 0
 
 # Pin Nummern vervenden statt GPIO Nummern
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
 # Set all relevant Pins to "HIGH" which switches all of - then CWL runs in level "1"
-#GPIO.setup(23, GPIO.OUT)
-#GPIO.setup(21, GPIO.OUT)
+GPIO.setup(23, GPIO.OUT)
+GPIO.setup(21, GPIO.OUT)
 
 
 Stufe_0 = GPIO.input(23)
 Stufe_3 = GPIO.input(21)
 if Stufe_0 == 0:
-  print ("Stufe 0")
+  if debug: 
+    print ("KWL Stufe 0") 
+  else:
+    print("0")
 elif Stufe_3 == 0:
-  print ("Stufe 3")
+  if debug: 
+    print ("KWL Stufe 3") 
+  else: 
+    print("3")
 else:
-  print ("Hmm - vermutlich Stufe 1")
+  if debug: 
+    print ("KWL Stufe 1") 
+  else: 
+    print("1")
 
 
 
-print("end")
+
+if debug: print("end")
